d46396159825952a4ca167e67db2c36600361136
alumne <alumne@proven>
2017-10-23 00:13:00 +0200

    modificat per primer cop

---
+++ PSP06-socketExercici6/src/ClientChat.java
@@ -0,0 +1,61 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+
+import java.net.*;
+import java.util.*;
+
+public class ClientChat {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        String entradaTeclat = "";
+        String entradaSocket = "";
+
+        try {
+            Socket socket = new Socket("localhost", 7000);
+
+            try {
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+                System.out.println("Envia un missatge al servidor, rebràs reposta (. per acabar)");
+                while (!".".equals(entradaTeclat) && !".".equals(entradaSocket)) {
+
+                    // LLegir del teclat (entrada stàndard)
+                    entradaTeclat = scan.nextLine();
+                    //enviar al servidor pel socket
+                    dos.writeUTF(entradaTeclat);
+
+                    // Un cop enviat el missatge al servidor, si no he enviat l'ordre de desconexió 
+                    // espero la resposta del Server i la mostro per pantalla
+                    if (!".".equals(entradaTeclat)) {
+                        //rebre resposta del servidor pel socket
+                        entradaSocket = dis.readUTF();
+                        
+                        if (!".".equals(entradaSocket)) {
+                            //mostrar la resposta en pantalla (sortida stàndard)
+                            System.out.println("Servidor respon: " + entradaSocket);
+                            
+                            //indicar al usuari que ja pot escriure
+                            System.out.println("Escriu nou missatge (. per acabar): ");
+                        }
+                        else {
+                            System.out.println("Connexió finalitzada pel servidor");
+                        }
+
+                    }
+
+                }
+            } catch (SocketException | EOFException ex) {
+                System.out.println("Servidor desconnectat");
+            }
+        } catch (SocketException ex2) {
+            System.out.println("No s'ha trobat el servidor");
+        }
+
+    }
+
+}
