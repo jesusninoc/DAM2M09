9a01e5931318b92b5c331831219a8fb1147c2087
Mercedes <mcastellon@puntsoft.com>
2017-10-22 20:00:03 +0200

    Afegir comentaris indicant els errors

--- PSP02-socketExercici1/src/main.java
+++ PSP02-socketExercici1/src/main.java
@@ -1,15 +1,16 @@
-
+//error 1: faltava fer l'import de java.net.Socket
 import java.io.IOException;
 import java.net.Socket;
 
 
 public class main {
 	public static void main(String[] args) {
+        //error 2: faltava tractar l'excepció capturant-la o fent un throw
             try {
                 Socket s = new Socket("127.0.0.1", 90);
                 s.close();
             } catch (IOException ex) {
-//                Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("error d'E/S: " + ex.getLocalizedMessage());
             }
 	}
 }
